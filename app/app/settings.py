92462187e14375e41788bb85c888a5bfef36ffcb
Iustina Dumitru <iustinadumitru21@gmail.com>
2021-11-14 02:10:20 +0200

    Add user & token serializers

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework.authtoken',
+
     'core',
+    'user'
 ]
 
 MIDDLEWARE = [
